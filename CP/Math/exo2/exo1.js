c429e9b0bfd720cf2582a94c825716b871e45bf9
youssef <yousdu061996@gmail.com>
2019-12-23 14:11:46 +0100

    frere tema l'exo 2 dans le dossier cp

---
+++ CP/Math/exo2/exo1.js
@@ -0,0 +1,32 @@
+﻿var tab = [];
+for (var i = 0; i <= 10; i++) {
+    tab.push(i);
+}
+
+
+var sol = Math.round(Math.random() * Math.floor(10));
+
+delete tab[sol];
+
+for (var i in tab) {
+    var x = document.getElementById(i);
+    x.innerHTML = i;
+}
+
+
+var inco = document.getElementById(sol);
+inco.innerHTML = '<input id="res">';
+
+function verification() {
+
+    var q = document.getElementById("res");
+
+    if (sol == q.value) {
+        alert("BRAVO");
+    }
+
+    else (
+        alert("mauvaise reponse")
+    )
+
+}
---
+++ CP/Math/exo2/exo2.js
@@ -0,0 +1,41 @@
+﻿var tab = [];
+
+
+for (var i = 1; i <= 3; i++) {
+
+    var m = document.getElementById(i);
+    var n = Math.floor(Math.random() * 10);
+    
+    m.src = "./im/image" + n + ".png";
+    tab.push(n + 1);
+   
+}
+
+
+function verification() {
+
+
+    var res = 0;
+    for (var i = 1; i <= 3; i = i + 1) {
+
+        var r = document.getElementById(3+i);
+
+        if (tab[i-1] == r.value) {
+            res = res + 1;
+            r.style.backgroundColor = "green";
+        }
+        else { r.style.backgroundColor = "red" }
+
+    }
+
+
+    if (res == 3) {
+        alert("Bravo tu as eu tout juste ")
+    }
+    else {
+        alert("dommage tu as fait " + (3 - res) + "erreurs , corrige toi")
+    }
+}
+
+
+
